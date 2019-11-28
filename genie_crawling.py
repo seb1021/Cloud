db5bf5cfb32c68f2c7814319a3aeabb433018b71
root <root@Ubuntu.yg2rmt2vopgublqjmg5j3xw2hc.psx.internal.cloudapp.net>
2019-11-28 10:32:49 +0000

    191128 genie top50 song title

--- genie_crawling.py
+++ genie_crawling.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3'}
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3'}#user info
 
 url = 'https://www.genie.co.kr/chart/top200'
 resp = requests.get(url,headers = headers)
+
 soup = BeautifulSoup(resp.text, 'html.parser')
 
-song = soup.find("a",{"class": "title ellipsis"}).text
 
-print(song)
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for song in songs:
+    title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
+    print(title.strip())#use strip() -> delete blank space
+
