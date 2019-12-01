489df4e01239b0b42362e3c2a45106640958e41d
root <root@ubuntu.sftwcx5dp0pudoy2askp0ptycg.syx.internal.cloudapp.net>
2019-12-01 15:55:41 +0000

    1202 bugs title+signer in .txt

--- bugs.py
+++ bugs.py
@@ -14,12 +14,11 @@ top_list2 = soup.select('#CHARTrealtime > table > tbody > tr > td > p > a')
 f_song = open("bugs_song.txt","w") #노래 차트 텍스트 파일
 f_singer = open("bugs_singer.txt","w") #가수 차트 텍스트 파일
 
-
-#for i in top_list:
- #   song = i.find('th').find('p').find('a')
-  #  print(song.text)
-   # data_song = "%s\n" % song.text
-    #f_song.write(data_song)
+for i in top_list:
+    song = i.find('th').find('p').find('a')
+    print(song.text)
+    data_song = "%s\n" % song.text
+    f_song.write(data_song)
 
 #for i in top_list2:
  #   singer = i.find('td').find('p').find('a')
@@ -32,4 +31,5 @@ for i in soup.find_all('p',class_='artist'):
     print(singer.text)
     data_singer = "%s\n" % singer.text
     f_singer.write(data_singer)
-    
+
+
