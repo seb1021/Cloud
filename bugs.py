b7b5ac3f584b0182849160c034c35b062f77038a
root <root@ubuntu.sftwcx5dp0pudoy2askp0ptycg.syx.internal.cloudapp.net>
2019-12-03 11:39:23 +0000

    1203 bugs crawling

--- bugs.py
+++ bugs.py
@@ -10,15 +10,21 @@ html = req.text
 soup = BeautifulSoup(html,'html.parser')
 
 top_list = soup.select('#CHARTrealtime > table > tbody > tr')
-top_list2 = soup.select('#CHARTrealtime > table > tbody > tr > td > p > a')
+top_list2 = soup.select('#CHARTrealtime > table > tbody > tr')
 f_song = open("bugs_song.txt","w") #노래 차트 텍스트 파일
 f_singer = open("bugs_singer.txt","w") #가수 차트 텍스트 파일
 
-for i in top_list:
-    song = i.find('th').find('p').find('a')
-    print(song.text)
+title = [None]*100
+singer = [None]*100
+n = 0
+
+for i in soup.find_all('p',class_='title'):
+    song = i.find('a')
+    # print(song.text)
     data_song = "%s\n" % song.text
-    f_song.write(data_song)
+    f_song.write(data_song) #텍스트파일에 노래 이름 저장
+    title[n] = song.text #배열에 노래 이름 저장
+    n = n+1
 
 #for i in top_list2:
  #   singer = i.find('td').find('p').find('a')
@@ -26,10 +32,15 @@ for i in top_list:
    # data_singer = "%s\n" % singer.text
     #f_singer.write(data.singer)
 
-for i in soup.find_all('p',class_='artist'):
-    singer = i.find('a')
-    print(singer.text)
+m = 0
+singer2 = [None]*100
+
+for z in soup.find_all('p',class_='artist'):
+    singer = z.find('a')
     data_singer = "%s\n" % singer.text
     f_singer.write(data_singer)
+    singer2[m] = singer.text #배열에 가수 이름 저장
+    m = m+1
 
-
+for i in range(0,100):
+    print("%d위 : %s - %s" %(i+1,title[i],singer2[i]))
